79fcf22949103f81bd2fcc7f7f8d95e3b551cb38
verbinna22 <mylogininya@yandex.ru>
2025-09-11 15:58:10 +0300

    add one point case

--- backend/main/src/segment.cpp
+++ backend/main/src/segment.cpp
@@ -1,4 +1,5 @@
 #include "segment.h"
+#include <algorithm>
 
 Segment::Segment(const Point &first, const Point &second)
     : _first(first),
@@ -15,6 +16,18 @@ const Point &Segment::get_second() const noexcept {
 
 bool Segment::is_intersecting(const Segment &route) const noexcept {
     // TODO refactor
+    if (route.get_first() == route.get_second()) {
+        if (get_first().get_y() == get_second().get_y()) {
+            int max_x = std::max(get_first().get_x(), get_second().get_x());
+            int min_x = std::min(get_first().get_x(), get_second().get_x());
+            return min_x <= route.get_first().get_x() &&
+                route.get_first().get_x() <= max_x;
+        }
+        int max_y = std::max(get_first().get_y(), get_second().get_y());
+        int min_y = std::min(get_first().get_y(), get_second().get_y());
+        return min_y <= route.get_first().get_y() &&
+            route.get_first().get_y() <= max_y;
+    }
     long long cross_product = (get_second() - get_first())
                         .cross_product(route.get_second() - route.get_first());
     if (cross_product != 0) {
--- backend/test/src/test_segment.cpp
+++ backend/test/src/test_segment.cpp
@@ -39,3 +39,55 @@ TEST(test_segment, is_intersecting__on_one_line_without_intersecting_points__ret
 
     ASSERT_TRUE(result);
 }
+
+TEST(test_segment, is_intersecting__on_one_line_without_intersection_point_and_segment__returns_false) {
+    Point start_one(0, 0);
+    Point finish_one(0, 2);
+    Point start_two(0, -1);
+    Point finish_two(0, -1);
+    Segment segment(start_one, finish_one);
+    Segment another_segment(start_two, finish_two);
+
+    bool result = segment.is_intersecting(another_segment);
+
+    ASSERT_FALSE(result);
+}
+
+TEST(test_segment, is_intersecting__on_one_line_with_intersection_point_and_segment__returns_true) {
+    Point start_one(0, 0);
+    Point finish_one(0, 2);
+    Point start_two(0, 0);
+    Point finish_two(0, 0);
+    Segment segment(start_one, finish_one);
+    Segment another_segment(start_two, finish_two);
+
+    bool result = segment.is_intersecting(another_segment);
+
+    ASSERT_TRUE(result);
+}
+
+TEST(test_segment, is_intersecting__on_one_line_with_intersection_and_without_common_point_point_and_segment__returns_true) {
+    Point start_one(0, 0);
+    Point finish_one(0, 2);
+    Point start_two(0, 1);
+    Point finish_two(0, 1);
+    Segment segment(start_one, finish_one);
+    Segment another_segment(start_two, finish_two);
+
+    bool result = segment.is_intersecting(another_segment);
+
+    ASSERT_TRUE(result);
+}
+
+TEST(test_segment, is_intersecting__on_one_line_with_intersection_and_point_and_segment_same_y__returns_false) {
+    Point start_one(0, 0);
+    Point finish_one(2, 0);
+    Point start_two(-1, 0);
+    Point finish_two(-1, 0);
+    Segment segment(start_one, finish_one);
+    Segment another_segment(start_two, finish_two);
+
+    bool result = segment.is_intersecting(another_segment);
+
+    ASSERT_FALSE(result);
+}
