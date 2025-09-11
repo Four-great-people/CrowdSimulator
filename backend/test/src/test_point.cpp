cd9c4fe87d985836a7c4f78b1f88c08f1f0c027d
verbinna22 <mylogininya@yandex.ru>
2025-09-12 00:51:03 +0300

    add point minus test

--- backend/test/src/test_point.cpp
+++ backend/test/src/test_point.cpp
@@ -21,3 +21,13 @@ TEST(test_point, get_y__reflect__returns_y) {
 
     ASSERT_EQ(result, y);
 }
+
+TEST(test_point, minus__basic__returns_point) {
+    Point p1(4, 8);
+    Point p2(2, -6);
+
+    Point p3 = p2 - p1;
+
+    ASSERT_EQ(p3.get_x(), -2);
+    ASSERT_EQ(p3.get_y(), -14);
+}
