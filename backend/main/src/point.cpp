215d31e0574456604021e15ad4414877249f5c5e
verbinna22 <mylogininya@yandex.ru>
2025-09-11 14:06:28 +0300

    implement point

--- backend/main/src/point.cpp
+++ backend/main/src/point.cpp
@@ -3,14 +3,12 @@
 Point::Point(int x, int y)
     : _x(x),
       _y(y)
-{
-    
-}
+{}
 
 int Point::get_x() const noexcept {
-    
+    return _x;
 }
 
 int Point::get_y() const noexcept {
-    
+    return _y;
 }
