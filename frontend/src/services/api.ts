0924f7870658256e915261c3bfc0077376dbc5c3
verbinna22 <mylogininya@yandex.ru>
2025-09-25 18:56:50 +0300

    add fake functions to frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,18 +2,7 @@ import { Grid } from '../models/Grid';
 
 export const saveMapToBackend = async (grid: Grid): Promise<string> => {
     try {
-        const requestData = grid.getDataForBackend();
-
-        // const response = await fetch("http://localhost:5000/maps", {
-        //     method: 'POST',
-        //     headers: {'Content-Type': 'application/json'},
-        //     body: JSON.stringify(requestData)
-        // });
-        // const data = await response.json();
-        // return data._id;
-        console.log(requestData);
-        return "0";
-        
+        return await saveToRealBackend(grid);
     } catch (error) {
         throw error;
     }
@@ -21,29 +10,39 @@ export const saveMapToBackend = async (grid: Grid): Promise<string> => {
 
 export const GetRoutesFromBackend = async (mapId: string): Promise<{id: number, route: string[]}[]> => {
     try {
-
-        // const response = await fetch("http://localhost:5000/maps/" + mapId + "/simulate", {method: 'POST'});
-        // const data = await response.json();
-        // return data;
-        // Заглушка ответа
-
-
-
-        return [
-
-
-            { "id": 1, "route": ["RIGHT", "RIGHT", "RIGHT"] },
-
-
-            { "id": 2, "route": ["RIGHT_UP", "RIGHT", "RIGHT", "RIGHT"] },
-
-
-            { "id": 3, "route": ["DOWN", "LEFT", "LEFT", "LEFT"] }
-
-
-        ];
-        
+        return await getRoutes(mapId);
     } catch (error) {
         throw error;
     }
 };
+
+async function getRoutes(mapId: string): Promise<{id: number, route: string[]}[]> {
+    const response = await fetch("http://localhost:5000/maps/" + mapId + "/simulate", { method: 'POST' });
+    const data = await response.json();
+    return data;
+}
+
+async function saveToRealBackend(grid: Grid): Promise<string> {
+    const requestData = grid.getDataForBackend();
+    const response = await fetch("http://localhost:5000/maps", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(requestData)
+    });
+    const data = await response.json();
+    return data._id;
+}
+
+function fakeGetRoutes() {
+    return [
+        { "id": 1, "route": ["RIGHT", "RIGHT", "RIGHT"] },
+        { "id": 2, "route": ["RIGHT_UP", "RIGHT", "RIGHT", "RIGHT"] },
+        { "id": 3, "route": ["DOWN", "LEFT", "LEFT", "LEFT"] }
+    ];
+}
+
+function fakeSave(grid: Grid) {
+    const requestData = grid.getDataForBackend();
+    console.log(requestData);
+    return "0";
+}
